49c2cd841eab1362d3c21388c41cae01ff025528
Karlos g liberal (patxangas) <karlosgliberal@gmail.com>
2015-08-30 14:21:58 +0200

    Cambio a lo bruto de todo

--- app.js
+++ app.js
@@ -1,58 +1,37 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var restify = require('express-restify-mongoose')
 
-var routes = require('./routes/index');
+mongoose.connect('mongodb://localhost/alertsome-rest');
+
+var User = new Schema({
+    id: { type: String },
+    notify_id: { type: String },
+    token: {type: String}
+});
+var UserModel = mongoose.model('User', User);
+
+var Group = new Schema({
+    notify_id: [User],
+    name: { type: String},
+    tipo: {type: String}
+});
+
+var GroupModel = mongoose.model('Group', Group);
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
 
-app.use('/', routes);
+var router = express.Router();
+restify.serve(router, UserModel);
+restify.serve(router, GroupModel);
+app.use(router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(3000, function() {
+    console.log("Express server listening on port 3000");
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
